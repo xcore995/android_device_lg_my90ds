5e6373e1a8713972d48e2e2c95889ef4447ca067
xcore995 <arman.p95@gmail.com>
2017-02-28 17:46:51 +0400

    clean up

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,14 +30,14 @@ BOARD_VOLD_MAX_PARTITIONS := 25
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
 
 # kernel stuff
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/tools/bootimg.mk
+#BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/tools/bootimg.mk
 MTK_PLATFORM := mt6582
 MTK_PROJECT := magna
 TARGET_KERNEL_SOURCE := kernel/lg/magna/kernel
 TARGET_KERNEL_CONFIG := magna_defconfig
 BOARD_KERNEL_CMDLINE :=
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x00000100
-
+#TARGET_PREBUILT_KERNEL := device/lg/magna/kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 

78f2f80ea764c6ee1b0670bb3e1b01193dccaf12
xcore995 <arman.p95@gmail.com>
2017-11-08 19:49:42 +0400

    my90ds: Build vibrator HAL

--- device.mk
+++ device.mk
@@ -142,6 +142,9 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
     librs_jni \
     com.android.future.usb.accessory
 

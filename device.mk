c0b77b0f48babe859587fb804ff60a2347d880ca
xcore995 <arman.p95@gmail.com>
2018-03-06 19:36:00 +0000

    my90ds: Add media codecs from MM

--- device.mk
+++ device.mk
@@ -64,7 +64,9 @@ PRODUCT_COPY_FILES += \
     device/lg/my90ds/rootdir/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
     device/lg/my90ds/rootdir/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
     device/lg/my90ds/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    device/lg/my90ds/rootdir/system/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    device/lg/my90ds/rootdir/system/etc/media_codecs_mediatek_audio.xml:system/etc/media_codecs_mediatek_audio.xml \
+    device/lg/my90ds/rootdir/system/etc/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_video.xml \
+    device/lg/my90ds/rootdir/system/etc/mtk_omx_core.cfg:system/etc/mtk_omx_core.cfg \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml

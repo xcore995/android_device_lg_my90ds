84539a68c26271800262036431520a210e7ce025
xcore995 <arman.p95@gmail.com>
2018-03-06 19:59:23 +0000

    my90ds: Auto apply patches

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,18 +1,15 @@
-#
-# Copyright 2015 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+cd frameworks/av
+git reset --hard && git clean -f -d
+patch -p1 < ../../device/lg/my90ds/patches/xcore/frameworks_av.patch
+cd ../..
+cd system/netd
+git reset --hard && git clean -f -d
+patch -p1 < ../../device/lg/my90ds/patches/netd.patch
+cd ../..
+cd system/core
+git reset --hard && git clean -f -d
+patch -p1 < ../../device/lg/my90ds/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+cd ../..
 
 add_lunch_combo lineage_my90ds-userdebug
 add_lunch_combo lineage_my90ds-eng

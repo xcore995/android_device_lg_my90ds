846e9a961d387e17b530e47924c8aebeda39cd89
xcore995 <arman.p95@gmail.com>
2018-05-13 12:23:53 +0000

    my90ds: Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -2,22 +2,37 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-PRODUCT_LOCALES := en_US ru_RU
+# call dalvik heap config
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Call device specific makefile
 $(call inherit-product, device/lg/my90ds/device.mk)
 
+# call the proprietary setup
+$(call inherit-product, vendor/lg/my90ds/my90ds-vendor.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_my90ds
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H502
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_DEVICE := my90ds
+PRODUCT_GMS_CLIENTID_BASE := android-lge
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=my90ds_global_com \
     TARGET_DEVICE=my90ds
+	
+TARGET_VENDOR_PRODUCT_NAME := my90ds
+TARGET_VENDOR_DEVICE_NAME := my90ds
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+BUILD_FINGERPRINT=lge/my90ds_global_com/my90ds:6.0/MRA58K/1619509518de2:user/release-keys \
+PRIVATE_BUILD_DESC="my90ds_global_com-user 6.0 MRA58K 1619509518de2 release-keys"
+
+TARGET_VENDOR := lge

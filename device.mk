ebbea03010d03d00e267e29ce6987f2affddf37f
xcore995 <arman.p95@gmail.com>
2018-07-04 16:26:38 +0400

    my90ds: Add sensors HAL

--- device.mk
+++ device.mk
@@ -99,10 +99,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl
     
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl
-
 
 # FMRadio
 PRODUCT_PACKAGES += \
@@ -134,6 +130,14 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl
+
+# Sensors HAL
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+    
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl

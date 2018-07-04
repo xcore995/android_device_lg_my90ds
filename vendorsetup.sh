4ab46073e90e5b242ce39e15f1a1a13caaafb005
xcore995 <arman.p95@gmail.com>
2018-07-04 16:28:56 +0400

    my90ds: Add Patch's

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,7 @@
+cd system/core
+git reset --hard && git clean -f -d
+patch -p1 < ../../device/lg/my90ds/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+patch -p1 < ../../device/lg/my90ds/patches/0001-Dont-reboot-to-bootloader.patch
+cd ../..
 add_lunch_combo lineage_my90ds-userdebug
 add_lunch_combo lineage_my90ds-eng

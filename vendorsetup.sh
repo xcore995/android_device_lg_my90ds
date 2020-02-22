2b4b9a383b4a81818a420f976796a405a217afe8
xcore995 <arman.p95@gmail.com>
2020-02-22 14:21:53 +0400

    Patch only framework/av

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,14 +2,6 @@ cd frameworks/av
 git reset --hard && git clean -f -d
 patch -p1 < ../../device/lg/my90ds/patches/xcore/frameworks_av.patch
 cd ../..
-cd system/netd
-git reset --hard && git clean -f -d
-patch -p1 < ../../device/lg/my90ds/patches/netd.patch
-cd ../..
-cd system/core
-git reset --hard && git clean -f -d
-patch -p1 < ../../device/lg/my90ds/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-cd ../..
 
 add_lunch_combo lineage_my90ds-userdebug
 add_lunch_combo lineage_my90ds-eng

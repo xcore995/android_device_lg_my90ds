cd0f0ae622c7894d7a9708dd4a7b68ea8d078090
xcore995 <arman.p95@gmail.com>
2020-03-29 16:15:22 +0400

    Add xlog shim patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,10 @@
 cd frameworks/av
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/lg/my90ds/patches/xcore/frameworks_av.patch
+patch -p1 < ../../device/lge/my90ds/patches/xcore/frameworks_av.patch
+cd ../..
+cd system/core
+git reset --hard && git clean -f -d
+patch -p1 < ../../device/lge/my90ds/patches/xcore/libcutils.patch
 cd ../..
 
 add_lunch_combo lineage_my90ds-userdebug

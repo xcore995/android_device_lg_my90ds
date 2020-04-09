e2ddc86a74ae2e4f628078b60b3331541ce9d357
xcore995 <arman.p95@gmail.com>
2020-04-09 09:18:47 -0400

    Update shim libs

--- libmagna/Android.mk
+++ libmagna/Android.mk
@@ -17,16 +17,10 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
     bionic.cpp \
-    ril.cpp \
-    ssl.c \
-    wvm.cpp \
-    ui.cpp \
     xlog.c \
-    mtkaudio.cpp \
-    camera.cpp
+    ui.cpp
 
-LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
-LOCAL_SHARED_LIBRARIES := libbinder libcrypto liblog libstagefright_foundation libui libgui libcutils libutils libc
+LOCAL_SHARED_LIBRARIES := libbinder libui libcutils libutils libc
 LOCAL_MODULE := libmagna
 LOCAL_MODULE_TAGS := optional
 

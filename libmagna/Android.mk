462380dc08a0542748c01c732ec9b6b7b6f136bf
xcore995 <arman.p95@gmail.com>
2020-04-25 11:43:47 -0400

    Update shim's

--- libmagna/Android.mk
+++ libmagna/Android.mk
@@ -1,28 +1,16 @@
-# Copyright (C) 2015 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
     bionic.cpp \
-    ril.cpp \
     xlog.c \
-    mtkaudio.cpp
+    ui.cpp \
+    icu.c \
+    camera.cpp \
+    buffer.cpp
 
-LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
-LOCAL_SHARED_LIBRARIES := libbinder libcrypto liblog libstagefright_foundation libui libgui libcutils libutils libc
+LOCAL_SHARED_LIBRARIES := libbinder libui libgui libcutils libutils libc liblog libicuuc libicui18n  libstagefright_foundation
 LOCAL_MODULE := libmagna
 LOCAL_MODULE_TAGS := optional
 
--- libmagna/gui.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdint.h>
-#include <ui/PixelFormat.h>
-
-extern "C" {
-    void _ZN7android13GraphicBufferC1Ejjij(void *instance, uint32_t inWidth, uint32_t inHeight, android::PixelFormat inFormat, uint32_t inUsage);
-}
